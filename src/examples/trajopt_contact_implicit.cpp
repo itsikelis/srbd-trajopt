af444a62b5de1ec93161f3eb827b7d972b89de67
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2024-06-05 18:30:15 +0300

    Add robot_dart visualisation

--- src/examples/trajopt_contact_implicit.cpp
+++ src/examples/trajopt_contact_implicit.cpp
@@ -26,11 +26,18 @@
 #include <trajopt/utils/types.hpp>
 #include <trajopt/utils/utils.hpp>
 
+#define VIZ 1
+
+#if (VIZ)
+#include <robot_dart/gui/magnum/graphics.hpp>
+#include <robot_dart/robot_dart_simu.hpp>
+#endif
+
 using Eigen::Vector3d;
 using Eigen::VectorXd;
 
-static constexpr size_t numKnots = 10;
-static constexpr size_t numSamples = 16;
+static constexpr size_t numKnots = 5;
+static constexpr size_t numSamples = 6;
 
 static constexpr double totalTime = 0.5;
 
@@ -39,7 +46,7 @@ static constexpr double initBodyPosX = 0.;
 static constexpr double initBodyPosY = 0.;
 static constexpr double initBodyPosZ = 0.5;
 
-static constexpr double targetBodyPosX = 0.2;
+static constexpr double targetBodyPosX = 0.;
 static constexpr double targetBodyPosY = 0.;
 static constexpr double targetBodyPosZ = 0.5;
 
@@ -184,38 +191,95 @@ int main()
     std::cout << "Wall clock time: " << duration.count() / 1000 << " seconds." << std::endl;
 
     // Print variables.
+    //
+    // double dt = 0.;
+    // for (size_t i = 0; i < numSamples; ++i) {
+    //     std::cout << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::BODY_POS_TRAJECTORY))->trajectoryEval(dt, 0).transpose() << std::endl;
+    //
+    //     dt += sampleTime;
+    // }
+    //
+    // std::cout << std::endl;
+    //
+    // for (size_t k = 0; k < 4; k++) {
+    //
+    //     dt = 0.;
+    //     for (size_t i = 0; i < numSamples; ++i) {
+    //         std::cout << "p_" << k << ": " << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::FOOT_POS + "_" + std::to_string(k)))->trajectoryEval(dt, 0).transpose() << std::endl;
+    //
+    //         std::cout << "v_" << k << ": " << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::FOOT_POS + "_" + std::to_string(k)))->trajectoryEval(dt, 1).transpose() << std::endl;
+    //
+    //         dt += sampleTime;
+    //     }
+    //
+    //     std::cout << std::endl;
+    //
+    //     dt = 0.;
+    //     for (size_t i = 0; i < numSamples; ++i) {
+    //         std::cout << "f_" << k << ": " << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::FOOT_FORCE + "_" + std::to_string(k)))->trajectoryEval(dt, 0).transpose() << std::endl;
+    //
+    //         dt += sampleTime;
+    //     }
+    //
+    //     std::cout << std::endl;
+    // }
 
-    double dt = 0.;
-    for (size_t i = 0; i < numSamples; ++i) {
-        std::cout << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::BODY_POS_TRAJECTORY))->trajectoryEval(dt, 0).transpose() << std::endl;
+#if (VIZ)
+    robot_dart::RobotDARTSimu simu(0.001);
+    auto graphics = std::make_shared<robot_dart::gui::magnum::Graphics>();
+    simu.set_graphics(graphics);
+    graphics->record_video(std::string(SRCPATH) + "srbd_implicit.mp4");
 
-        dt += sampleTime;
+    simu.add_floor();
+    // auto terrain = std::make_shared<robot_dart::Robot>(std::string(SRCPATH) + "/step-terrain.urdf");
+    // simu.add_visual_robot(terrain);
+
+    auto robot = robot_dart::Robot::create_box(Eigen::Vector3d(0.6, 0.2, 0.15), Eigen::Isometry3d::Identity());
+    robot->free_from_world();
+    robot->set_ghost(false);
+    robot->set_cast_shadows(true);
+    simu.add_robot(robot);
+    robot->skeleton()->setMobile(false);
+
+    for (size_t k = 0; k < model.numFeet; ++k) {
+
+        auto foot = robot_dart::Robot::create_ellipsoid(Eigen::Vector3d(0.05, 0.05, 0.05), Eigen::Isometry3d::Identity(), "free", 0.1, dart::Color::Red(1.0), "foot" + std::to_string(k));
+        foot->set_ghost(false);
+        foot->set_cast_shadows(true);
+        foot->skeleton()->setMobile(false);
+        simu.add_robot(foot);
     }
 
-    std::cout << std::endl;
+    // Visualise trajectory.
+    double dt = 0.001;
+    size_t iters = totalTime / dt + 1;
 
-    for (size_t k = 0; k < 4; k++) {
+    double t = 0.;
+    for (size_t i = 0; i < iters; ++i) {
+        Vector3d bodyPos = std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::BODY_POS_TRAJECTORY))->trajectoryEval(t, 0);
+        Vector3d bodyRot = std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::BODY_ROT_TRAJECTORY))->trajectoryEval(t, 0);
 
-        dt = 0.;
-        for (size_t i = 0; i < numSamples; ++i) {
-            std::cout << "p_" << k << ": " << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::FOOT_POS + "_" + std::to_string(k)))->trajectoryEval(dt, 0).transpose() << std::endl;
+        Eigen::AngleAxisd z_rot(bodyRot[0], Eigen::Vector3d::UnitZ());
+        Eigen::AngleAxisd y_rot(bodyRot[1], Eigen::Vector3d::UnitY());
+        Eigen::AngleAxisd x_rot(bodyRot[2], Eigen::Vector3d::UnitX());
+        auto tf = Eigen::Isometry3d::Identity();
+        tf.rotate(z_rot);
+        tf.rotate(y_rot);
+        tf.rotate(x_rot);
 
-            std::cout << "v_" << k << ": " << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::FOOT_POS + "_" + std::to_string(k)))->trajectoryEval(dt, 1).transpose() << std::endl;
+        auto rot = dart::math::logMap(tf);
+        simu.robot(1)->set_positions(robot_dart::make_vector({rot[0], rot[1], rot[2], bodyPos[0], bodyPos[1], bodyPos[2]}));
 
-            dt += sampleTime;
+        for (size_t k = 0; k < model.numFeet; ++k) {
+            Vector3d footPos = std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::FOOT_POS + "_" + std::to_string(k)))->trajectoryEval(t, 0);
+            simu.robot(k + 2)->set_positions(robot_dart::make_vector({0., 0., 0., footPos[0], footPos[1], footPos[2] + 0.025}));
         }
 
-        std::cout << std::endl;
-
-        dt = 0.;
-        for (size_t i = 0; i < numSamples; ++i) {
-            std::cout << "f_" << k << ": " << std::static_pointer_cast<trajopt::TrajectoryVars>(nlp.GetOptVariables()->GetComponent(trajopt::FOOT_FORCE + "_" + std::to_string(k)))->trajectoryEval(dt, 0).transpose() << std::endl;
-
-            dt += sampleTime;
-        }
-
-        std::cout << std::endl;
+        simu.step_world();
     }
+    simu.run(1.);
+
+#endif
 
     return 0;
 }
